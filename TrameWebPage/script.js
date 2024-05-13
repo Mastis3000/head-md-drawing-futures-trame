cb71ad582a461e357c26e20d355befb3642ccb9f
abstractmachine <douglas@abstractmachine.net>
2024-05-13 15:08:59 +0200

    Working tts

--- TrameWebPage/script.js
+++ TrameWebPage/script.js
@@ -1,11 +1,35 @@
-
-let story;
-let typeDelay = 10;
-let text = "Et Harmonie chercha sa prochaine couleur pour recolorier le monde.";
+let paragraph;
+let typeDelay = 40;
+let text = "";
+let subtext = "";
+let lastPhrase = "";
+let started = false;
+let story = {};
 
 function start() {
 
-    story = document.getElementById("story");
+    // get the story element
+    paragraph = document.getElementById("paragraph");
+
+    loadStory();
+
+    text = "Et Harmonie chercha sa prochaine couleur pour recolorier le monde."
+    raconter(text, true);
+
+}
+
+
+function loadStory() {
+
+    fetch('./story.json')
+        .then(response => response.json())
+        .then(data => story = data)
+        .catch(error => console.log(error));
+
+}
+
+
+function startSpeech() {
     // type out next text with the new text
     typeText(text, false);
     // speak it
@@ -19,12 +43,12 @@ function typeText(newText, append=false) {
 
     // empty out previous text
     if (!append) {
-        story.innerHTML = "";
+        paragraph.innerHTML = "";
     }
 
     let i = 0;
     let interval = setInterval(function() {
-        story.innerHTML += text.charAt(i);
+        paragraph.innerHTML += newText.charAt(i);
         i++;
         if (i > newText.length) {
             clearInterval(interval);
@@ -34,19 +58,66 @@ function typeText(newText, append=false) {
 
 
 // use Chrome Text-To-Speech to read the text
-function speak(text) {
+function speak(newText, newSubtext = "") {
+
+    text = newText;
+    subtext = newSubtext;
+
     let utterance = new SpeechSynthesisUtterance(text);
     utterance.lang = "fr-FR";
+    utterance.addEventListener("end", (event) => {
+        console.log(
+          speakSupplement(),
+        );
+      });
     speechSynthesis.speak(utterance);
+
 }
 
 
-// get access to serial port
-function getSerialPort() {
-    // request serial port access
-    navigator.serial.requestPort().then((port) => {
-        console.log("Serial port opened: " + port.getInfo().usbProductId);
-    }).catch((err) => {
-        console.error("Error opening serial port: " + err);
-    });
+
+function speakSupplement() {
+
+    if (subtext) {
+        let utterance = new SpeechSynthesisUtterance(subtext);
+        utterance.lang = "fr-FR";
+        speechSynthesis.speak(utterance);
+    }
+
 }
+
+
+
+
+// get keypress events
+document.addEventListener("keypress", (event) => {
+
+    if (!started) return;
+    
+    // if 'r' is pressed, red color
+    if (event.key === "r") {
+        action("rouge");
+    } else if (event.key === "v") {
+        // if 'v' is pressed, green color
+        action("vert");
+    } else if (event.key === "b") {
+        // if 'b' is pressed, blue color
+        action("bleu");
+    } else if (event.key === "m") {
+        // if 'm' is pressed, magenta color
+        action("magenta");
+    } else if (event.key === "j") {
+        // if 'y' is pressed, yellow color
+        action("jaune");
+    }
+
+});
+
+
+document.addEventListener("click", () => {
+
+    if (started) return;
+    start();
+    started = true;
+
+  });
---
+++ TrameWebPage/story.js
@@ -0,0 +1,23 @@
+let storyTimeout;
+
+function action(color) {
+
+    // choose a random phrase from the story for this color
+    let phrases = story.actions[color];
+
+    // choose one at random
+    let phrase = phrases[Math.floor(Math.random() * phrases.length)];
+
+    raconter(phrase);
+
+}
+
+
+function raconter(phrase) {
+
+    typeText(phrase, false);
+    speak(phrase);
+
+    lastPhrase = phrase;
+
+}
